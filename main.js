d0776548c62159878bd9f7082aa431182afaf86e
SusRobo <113228306+SusRobo@users.noreply.github.com>
2022-11-19 14:47:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(550, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log("Anything is initialized");
+}
+function draw()
+{
+    background("blue"); 
+    document.getElementById("square_side").innerHTML = "Width and height of a square will be = " + difference +"px";
+    fill('purple');
+    stroke('red');
+    square(noseX, noseY, difference);  
+}
+function gotPoses(results)
+{
+    if( results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(noseX, noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX  - rightWristX);
+        console.log(leftWristX, rightWristX, difference);
+    }
+}
